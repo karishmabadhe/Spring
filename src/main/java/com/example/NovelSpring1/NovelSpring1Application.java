63b57475b2f77ea66dbb4d7c3161fa8c23361c36
karishmabadhe <ksbadhe2012@gmail.com>
2022-02-02 11:23:40 +0530

    first commit

---
+++ src/main/java/com/example/NovelSpring1/NovelSpring1Application.java
@@ -0,0 +1,13 @@
+package com.example.NovelSpring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelSpring1Application.class, args);
+	}
+
+}
